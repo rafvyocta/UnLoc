f48b4c784c890386957dc1d97e98768b28998873
rafvyocta <rafvyoctanugraha@gmail.com>
2021-10-05 13:15:07 +0700

    First Code

---
+++ app/src/main/java/com/example/tbptb/login.java
@@ -0,0 +1,21 @@
+package com.example.tbptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void kembali(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/tbptb/signup.java
@@ -0,0 +1,26 @@
+package com.example.tbptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+    public void kembali(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    public void daftar(View view) {
+        startActivity(new Intent(this, login.class));
+        finish();
+    }
+}
